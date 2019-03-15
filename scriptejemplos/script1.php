9ff9a099f6b0711ec155fbfd940be2f45d489ce7
xavier <xavi.ort7@gmail.com>
2019-03-14 22:00:38 -0500

    script

---
+++ scriptejemplos/script1.php
@@ -0,0 +1,21 @@
+<center> MI PRIMER SCRIPT</center>
+<?php
+
+//mi primer script en PHP
+// desarrollado por: autor
+
+$nombre="joan";
+$apellido="ayala";
+$email="joan@gmail.com";
+
+echo"<center>html en php</center>";
+
+echo "Bienvenido".$nombre." ".$apellido;
+
+echo $nombre."<br>";
+echo $apellido."<br>";
+echo $email."<br>";
+
+
+?>
+<center> FIN DEL SCRIPT</center>
