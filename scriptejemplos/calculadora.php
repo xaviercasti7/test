4ac1081cc6c386e6606be71c235f346a69b66681
xavier <xavi.ort7@gmail.com>
2019-03-15 20:11:56 -0500

    calculadora

---
+++ scriptejemplos/calculadora.php
@@ -0,0 +1,31 @@
+<?php
+
+$num1=$_POST0['N1'];
+$num2=$_POST0['N2'];
+$oper=$_POST0['tipo_op'];	
+$num1=$_POST0['N1'];
+$resul=0;
+echo "mumero1:".$num1."."<br>";	
+echo "mumero2:".$num2."."<br>";	
+echo "tipo opreracion:".$oper;					
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
